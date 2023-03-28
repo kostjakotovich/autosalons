f884e66a10fb4812d0c2b125aead621888ae2b72
kostjakotovich <01DPKKotovics@rvt.lv>
2023-03-28 15:23:51 +0300

    Profila lapas izlabosana

--- profile.php
+++ profile.php
@@ -2,7 +2,7 @@
 
 require_once 'connection.php';
 
-$sql = "SELECT commentID, name, email, comment FROM comments";
+$sql = "SELECT userID, username, email, password FROM user";
 $conn = mysqli_connect($servername, $DBusername, $DBpassword, $dbname);
 $result = mysqli_query($conn, $sql);
 $row = mysqli_fetch_assoc($result);
@@ -15,7 +15,7 @@ $row = mysqli_fetch_assoc($result);
       <script src="../autosalons/js/script.js" defer></script>
     </head>
     <body>
-        <button class="button" id="redirecttoindex" onClick="RedToComments()">Profils</button>
-        <?php echo "<h1>Name: <h1>" . $row["name"]; ?>
+        <button class="button" id="redirecttoindex" onClick="RedToComments()">Home page</button>
+        <?php echo "<h1>Name: <h1>" . $row["username"]; ?>
         <?php echo "<h2>Email: <h2>" . $row["email"]; ?>
     </body>
