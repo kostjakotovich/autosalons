8d7b6147a4d0a310a2c19c52418dc33d77e643ea
Konstantīns Kotovičs <kostja.kotovich@gmail.com>
2023-04-17 00:10:44 +0300

    Pievienoju dizainu forum lapai

--- forum.php
+++ forum.php
@@ -23,27 +23,32 @@ if (isset($_POST['comment'])) {
 <head>
     <!-- style css link -->
     <link rel="stylesheet" href="css/forum.css">
-
 </head>
 <body>
     <?php require 'header.php'; ?>
 
-    <!-- Форма для добавления нового комментария -->
-    <form method="post">
-        <label for="comment">Оставьте свой комментарий:</label><br>
-        <textarea name="comment" id="comment" cols="30" rows="10"></textarea><br>
-        <input type="submit" value="Отправить">
-    </form>
+    <div class="container">
+        <div class="card">
+            <form method="post">
+                <label for="comment">Leave your comment (no more than 250 characters):</label><br>
+                <textarea name="comment" id="comment" cols="30" rows="5" maxlength="250"></textarea><br>
+                <input type="submit" value="Send comment">
+            </form>
+        </div>
+    </div>
 
-    <?php
-    // Отображаем комментарии и имена пользователей
-    foreach ($comments as $comment) {
-        echo '<div>';
-        echo '<h4>' . $comment['username'] . ' ' . $comment['date'] . '</h4>';
-        echo '<p>' . $comment['comment'] . '</p>';
-        echo '</div>';
-    }
-    ?>
+    <div class="comments">
+            <?php
+            // Отображаем комментарии и имена пользователей
+            foreach ($comments as $comment) {
+                echo '<div class="comment">';
+                echo '<h4>' . $comment['username'] . '</h4>';
+                echo '<p>' . $comment['comment'] . '</p><br>';
+                echo '<h4>Posted on ' . date('F j, Y', strtotime($comment['date'])) . '</h4>';
+                echo '</div>';
+            }
+            ?>
+        </div>
 
 </body>
 </html>
