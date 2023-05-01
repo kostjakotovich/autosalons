4db4f73d9dcfd0773d32b084ef7f4e93d46c982a
Konstantīns Kotovičs <kostja.kotovich@gmail.com>
2023-05-01 15:52:38 +0300

    Uzlaboju automasinas meklesanas funkciju pec modeles un markas kopa

--- phpSearchOption.php
+++ phpSearchOption.php
@@ -3,7 +3,7 @@ require_once 'connection.php';
 
 class SearchOption extends Database{
     public function searchOffers($search, $column) {
-      $query = "SELECT * FROM offers WHERE manufacturer LIKE :search OR type LIKE :search";
+      $query = "SELECT * FROM offers WHERE manufacturer LIKE :search OR type LIKE :search OR CONCAT(manufacturer, ' ', type) LIKE :search OR CONCAT(manufacturer, type) LIKE :search";
       $stmt = $this->connect()->prepare($query);
       $stmt->execute(['search' => '%'.$search.'%']);
       $result = $stmt->fetchAll();
