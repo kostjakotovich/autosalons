d82161636b9d01ff58930f58c3083e6e91b37d48
Konstantīns Kotovičs <kostja.kotovich@gmail.com>
2024-02-06 14:18:10 +0200

    Pievienots saraksts (masīvs) ar krāsām

---
+++ includes/car_colors.php
@@ -0,0 +1,16 @@
+<?php $carColors = [
+    "Black",
+    "Blue",
+    "Brown",
+    "Gray",
+    "Green",
+    "Orange",
+    "Pink",
+    "Purple",
+    "Red",
+    "Silver",
+    "White",
+    "Yellow",
+    "Custom", // Custom colors
+];
+?>
