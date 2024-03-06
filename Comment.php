9a6e1728696476bdcde89627075bee9eaee80678
Konstantīns Kotovičs <kostja.kotovich@gmail.com>
2024-03-06 14:35:02 +0200

    Pievienoju reply sistemu, tagad uz visiem komentariem var atbildet

--- Comment.php
+++ Comment.php
@@ -1,5 +1,6 @@
 <?php
 require_once 'connection.php';
+require_once 'user.php';
 
 class Comment {
     private $conn;
@@ -29,29 +30,19 @@ class Comment {
 
     
 
-    public function getTotalCommentsCount() {
-    $sql = "SELECT COUNT(*) as total FROM comments";
+    public function getTotalOriginalCommentsCount() {
+    $sql = "SELECT COUNT(*) as total FROM comments WHERE comments.parent_comment_id IS NULL";
     $stmt = $this->conn->query($sql);
     $result = $stmt->fetch(PDO::FETCH_ASSOC);
     return $result['total'];
-}
-
-
-    public function getTotalCommentsCountForPage($startIndex, $endIndex) {
-        $sql = "SELECT COUNT(*) as count FROM comments LIMIT :startIndex, :endIndex";
-        $stmt = $this->conn->prepare($sql);
-        $stmt->bindParam(':startIndex', $startIndex, PDO::PARAM_INT);
-        $stmt->bindParam(':endIndex', $endIndex, PDO::PARAM_INT);
-        $stmt->execute();
-        $row = $stmt->fetch(PDO::FETCH_ASSOC);
-        return $row['count'];
     }
-    
 
-    public function getCommentsForPage($startIndex, $commentsPerPage) {
+    // Метод для получения оригинальных комментариев
+    public function getOriginalCommentsForPage($startIndex, $commentsPerPage) {
         $sql = "SELECT comments.commentID, comments.userID, comments.comment, user.username, comments.date
                 FROM comments 
-                Left JOIN user ON comments.userID = user.userID 
+                LEFT JOIN user ON comments.userID = user.userID 
+                WHERE comments.parent_comment_id IS NULL 
                 ORDER BY comments.commentID DESC
                 LIMIT :startIndex, :commentsPerPage";
 
@@ -81,6 +72,12 @@ class Comment {
         date_default_timezone_set('Europe/Riga');
         $date = date("Y-m-d H:i:s");
     
+        // Получаем username пользователя, который оставил ответ
+        $replyUsername = $this->getUsernameByUserID($userID);
+    
+        // Получаем userID пользователя оригинального комментария
+        $originalCommentUserID = $this->getUserIDForOriginalComment($parentCommentID);
+    
         $sql = "INSERT INTO comments (comment, userID, date, parent_comment_id) VALUES (:comment, :userID, :date, :parentCommentID)";
         $stmt = $this->conn->prepare($sql);
         $stmt->bindParam(':comment', $comment);
@@ -88,29 +85,89 @@ class Comment {
         $stmt->bindParam(':date', $date);
         $stmt->bindParam(':parentCommentID', $parentCommentID);
         $stmt->execute();
+    
+        // Вставляем уведомление в таблицу
+        $notificationText = "You have received an answer from '$replyUsername'. Comment '$comment'.Check <a href='forum.php'>forum</a>.";
+        $insertNotification = $this->conn->prepare("INSERT INTO notifications (userID, message) VALUES (:userID, :message)");
+        $insertNotification->execute(array(':userID' => $originalCommentUserID, ':message' => $notificationText));
     }
     
+    
+    
+    // Метод для получения ответов на комментарии
     public function getRepliesForComment($parentCommentID) {
-        $sql = "SELECT * FROM comments WHERE parent_comment_id = :parentCommentID";
+        $replies = array();
+        $this->getRepliesRecursive($parentCommentID, $replies);
+        return $replies;
+    }
+
+    // Рекурсивная функция для получения всех ответов на комментарии
+    private function getRepliesRecursive($parentCommentID, &$replies) {
+        $sql = "SELECT comments.commentID, comments.userID, user.username, comments.comment, comments.date 
+                FROM comments 
+                INNER JOIN user ON comments.userID = user.userID 
+                WHERE comments.parent_comment_id = :parentCommentID
+                ORDER BY comments.date ASC"; // Сортировка по дате в возрастающем порядке (новые внизу)
+        
         $stmt = $this->conn->prepare($sql);
         $stmt->bindParam(':parentCommentID', $parentCommentID, PDO::PARAM_INT);
         $stmt->execute();
-    
-        $replies = array();
     
         while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
             $reply = array(
                 'commentID' => $row['commentID'],
                 'userID' => $row['userID'],
-                'username' => $row['username'], // Если у вас есть поле username в таблице комментариев
+                'username' => $row['username'],
                 'comment' => $row['comment'],
                 'date' => $row['date']
             );
     
             $replies[] = $reply;
-        }
     
-        return $replies;
+            // Рекурсивно вызываем эту функцию для поиска ответов на этот ответ
+            $this->getRepliesRecursive($row['commentID'], $replies);
+        }
     }
+    
+    
+    // метод для получения имени пользователя оригинального комментария
+    public function getUsernameForOriginalComment($commentID) {
+        $query = "SELECT u.username 
+                FROM user u 
+                JOIN comments c ON u.userID = c.userID 
+                WHERE c.commentID = (
+                    SELECT parent_comment_id 
+                    FROM comments 
+                    WHERE commentID = :commentID
+                )";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':commentID', $commentID);
+        $stmt->execute();
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+        return $result['username'];
+    }
+
+    // метод для получения ID пользователя оригинального комментария
+    public function getUserIDForOriginalComment($parentCommentID) {
+        $query = "SELECT c.userID 
+                FROM comments c 
+                WHERE c.commentID = :parentCommentID";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':parentCommentID', $parentCommentID);
+        $stmt->execute();
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+        return $result['userID'];
+    }
+    
+
+    public function getUsernameByUserID($userID) {
+        $query = "SELECT username FROM user WHERE userID = :userID";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':userID', $userID);
+        $stmt->execute();
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+        return $result['username'];
+    }
+    
     
 }
--- forum.php
+++ forum.php
@@ -13,9 +13,8 @@ $startIndex = 0;
 $endIndex = $startIndex + $commentsPerPage;
 // Определяем общее количество страниц, которые будут отображаться
 
-$totalComments = $commentObj->getTotalCommentsCount();
+$totalComments = $commentObj->getTotalOriginalCommentsCount();
 $totalPages = ceil($totalComments / $commentsPerPage);
-
 
 // Получаем текущую страницу из параметров URL
 if (isset($_GET['page'])) {
@@ -37,8 +36,7 @@ if ($currentPage == $totalPages) {
 }
 
 // Получаем комментарии для текущей страницы
-$comments = $commentObj->getCommentsForPage($startIndex, $commentsPerPage);
-
+$comments = $commentObj->getOriginalCommentsForPage($startIndex, $commentsPerPage);
 
 // Обработка формы для добавления нового комментария
 if (isset($_POST['comment'])) {
@@ -49,13 +47,24 @@ if (isset($_POST['comment'])) {
     exit;
 }
 
-// Получение ID с комментраия, для его удаления
+// Получение ID с комментария для его удаления
 if (isset($_POST['delete'])) {
     $commentID = $_POST['commentID'];
     $commentObj->deleteComment($commentID);
     header("Location: forum.php"); // перезагрузка страницы для избежания повторной отправки формы
     exit;
 }
+
+// Получение ID комментария для ответа
+if (isset($_POST['reply'])) {
+    $reply = $_POST['reply'];
+    $userID = $_SESSION['userID'];
+    $commentID = $_POST['parentCommentID']; // Используем parentCommentID
+    $commentObj->addReply($reply, $userID, $commentID);
+    header("Location: forum.php"); // перезагрузка страницы для избежания повторной отправки формы
+    exit;
+}
+
 
 ?>
 
@@ -79,36 +88,78 @@ if (isset($_POST['delete'])) {
         </div>
 
         <div class="comments">
-        <?php
-            foreach ($comments as $comment) {
-                echo '<div class="comment">';
-                echo '<h4>' . $comment['username'] . '</h4>';
-                echo '<p>' . $comment['comment'] . '</p><br>';
-                echo '<h4>Posted on ' . date('F j, Y', strtotime($comment['date'])) . '</h4>';
-                if (isset($_SESSION['userID']) && $_SESSION['roleID'] == 0 && $_SESSION['userID'] == $comment['userID']) {
-                    echo '<div class="card2">';
-                    echo '  <form method="post">';
-                    echo '      <input type="hidden" name="commentID" value="' . $comment['commentID'] . '">';
-                    echo '      <input type="submit" value="Delete" name="delete">';
-                    echo '  </form>';
-                    echo '</div>';
-                }
+            <?php foreach ($comments as $comment) { ?>
+                <div class="comment">
+                    <h4><?php echo $comment['username']; ?></h4>
+                    <p><?php echo $comment['comment']; ?></p><br>
+                    <h4>Posted on <?php echo date('F j, Y', strtotime($comment['date'])); ?></h4>
+                    <!-- Кнопки "Reply" и "Delete" на одном уровне по горизонтали -->
+                    <div class="button-group">
+                        <button class="reply-btn">Reply</button>
+                        <?php if (isset($_SESSION['userID']) && ($_SESSION['roleID'] == 0 && $_SESSION['userID'] == $comment['userID']) || $_SESSION['roleID'] == 1) { ?>
+                            <div class="card2">
+                                <form method="post">
+                                    <input type="hidden" name="commentID" value="<?php echo $comment['commentID']; ?>">
+                                    <input type="submit" value="Delete" name="delete">
+                                </form>
+                            </div>
+                        <?php } ?>
+                    </div>
+                    <!-- Форма для ответа на комментарий -->
+                    <div class="reply-form" style="display: none;">
+                        <form method="post">
+                            <input type="hidden" name="parentCommentID" value="<?php echo $comment['commentID']; ?>">
+                            <label for="reply_<?php echo $comment['commentID']; ?>">Your reply:</label><br>
+                            <textarea name="reply" id="reply_<?php echo $comment['commentID']; ?>" cols="30" rows="3" maxlength="250" required></textarea><br>
+                            <input type="submit" value="Send reply">
+                        </form>
+                    </div>
 
-                if (isset($_SESSION['roleID']) && $_SESSION['roleID'] == 1) {
-                    echo '<div class="card2">';
-                    echo '  <form method="post">';
-                    echo '      <input type="hidden" name="commentID" value="' . $comment['commentID'] . '">';
-                    echo '      <input type="submit" value="Delete" name="delete">';
-                    echo '  </form>';
-                    echo '</div>';
-                }
+                </div>
 
-                echo '</div>';
-            }
-        ?>
+                <?php $replies = $commentObj->getRepliesForComment($comment['commentID']); ?>
+                <?php if (!empty($replies)) { ?>
+                    <?php foreach ($replies as $reply) { ?>
+                        <div class="reply-comment">
+                            <div class="reply-container">
+                                <div class="reply">
+                                    <div class="reply-header">
+                                        <h4 class="reply-username"><?php echo $reply['username']; ?></h4>
+                                        <p class="reply-info">Reply to: <strong><?php echo $commentObj->getUsernameForOriginalComment($reply['commentID']); ?></strong></p>
+                                    </div>
+                                    <p class="reply-comment-text"><?php echo $reply['comment']; ?></p>
+                                    <br>
+                                    <h4 class="reply-date">Posted on <?php echo date('F j, Y', strtotime($reply['date'])); ?></h4>
+                                    <div class="button-group">
+                                        <button class="reply-btn">Reply</button>
+                                        <?php if (isset($_SESSION['userID']) && ($_SESSION['roleID'] == 0 && $_SESSION['userID'] == $comment['userID']) || $_SESSION['roleID'] == 1) { ?>
+                                            <div class="card2">
+                                                <form method="post">
+                                                    <input type="hidden" name="commentID" value="<?php echo $comment['commentID']; ?>">
+                                                    <input type="submit" value="Delete" name="delete">
+                                                </form>
+                                            </div>
+                                        <?php } ?>
+                                    </div>
+                                    <!-- Форма для ответа на комментарий -->
+                                    <div class="reply-form" style="display: none;">
+                                        <form method="post">
+                                            <input type="hidden" name="parentCommentID" value="<?php echo $reply['commentID']; ?>"> 
+                                            <label for="reply_<?php echo $comment['commentID']; ?>">Your reply:</label><br>
+                                            <textarea name="reply" id="reply_<?php echo $comment['commentID']; ?>" cols="30" rows="3" maxlength="250" required></textarea><br>
+                                            <input type="submit" value="Send reply">
+                                        </form>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    <?php } ?>
+  
+                <?php } ?>
+            <?php } ?>
+
         </div>
-
-
 
         <div class="pagination">
             <?php
@@ -123,6 +174,19 @@ if (isset($_POST['delete'])) {
             ?>
         </div>
     </div>
-<?php include 'footer.php'; ?>
+    <?php include 'footer.php'; ?>
+    
+    <script>
+            // JavaScript для управления отображением формы ответа
+            document.querySelectorAll('.reply-btn').forEach(function(btn) {
+            btn.addEventListener('click', function() {
+                var parentContainer = this.parentNode.parentNode;
+                var replyForm = parentContainer.querySelector('.reply-form');
+                if (replyForm) {
+                    replyForm.style.display = replyForm.style.display === 'none' ? 'block' : 'none';
+                }
+            });
+        });
+    </script>
 </body>
 </html>
